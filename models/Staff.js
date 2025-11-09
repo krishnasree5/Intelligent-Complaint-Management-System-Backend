8ed26c331ebf1411f27c069c1ec22f3bda2a5994
{Krishna <gkrishnasree5@gmail.com>
2025-11-09 18:32:09 +0530

    feat: create Staff and Admin schema

---
+++ models/Staff.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const staffSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  district: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "District",
+    required: true,
+  },
+  addedByAdmin: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Admin",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Staff", staffSchema);
