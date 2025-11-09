45de428715e1963d8ebe938b4260845c0ddfe269
{Krishna <gkrishnasree5@gmail.com>
2025-11-09 18:30:39 +0530

    feat: modify User schema to include otp authentication

--- models/User.js
+++ models/User.js
@@ -1,23 +1,40 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      sparse: true, // allows null values to not violate unique constraint
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     district: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-}, { timestamps: true });
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    otp: String,
+    otpExpires: Date,
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model("User", userSchema);
