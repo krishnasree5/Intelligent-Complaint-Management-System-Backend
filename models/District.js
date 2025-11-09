adab4d541e5aee1ec6c044bfc67b6a30bb846380
{Krishna <gkrishnasree5@gmail.com>
2025-11-09 18:53:57 +0530

    feat: remove state column from District schema

--- models/District.js
+++ models/District.js
@@ -4,11 +4,6 @@ const districtSchema = new mongoose.Schema({
   name: {
     type: String,
   },
-  state: {
-    type: String,
-    required: true,
-    trim: true,
-  },
 });
 
 export default mongoose.model("District", districtSchema);
