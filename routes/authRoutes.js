668a48fc7124cb83aef2d25cf6338e93e53d9d6b
{Krishna <gkrishnasree5@gmail.com>
2025-11-10 12:35:49 +0530

    feat: migrate to otp verification

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import {
+  login,
+  initiateRegistration,
+  verifyOtp,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", initiateRegistration);
+router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
 
 export default router;
